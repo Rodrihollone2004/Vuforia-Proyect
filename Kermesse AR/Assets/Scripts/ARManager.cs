3fb27c122ddf7a732c594c0d5e5810da2e705227
Rodrihollone2004 <rodri.g.fretes@gmail.com>
2026-04-19 14:57:05 -0300

    Rotation AR

--- Kermesse AR/Assets/Scripts/ARManager.cs
+++ Kermesse AR/Assets/Scripts/ARManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ARManager : MonoBehaviour
 {
     public GameObject gamesContent;
+    [SerializeField] private float rotationSpeed = 5f;
 
     private List<GameObject> allGames = new List<GameObject>();
     private int indexGame;
+    private float targetRotationZ = 0f;
 
     private void Start()
     {
@@ -15,40 +17,41 @@ public class ARManager : MonoBehaviour
 
         foreach (Transform child in gamesContent.transform)
         {
-            GameObject gameObj = child.gameObject;
+            allGames.Add(child.gameObject);
 
-            allGames.Add(gameObj);
-
-            gameObj.SetActive(false);
+            child.gameObject.SetActive(true);
         }
 
-        if (allGames.Count > 0)
-            allGames[indexGame].SetActive(true);
+        targetRotationZ = gamesContent.transform.localRotation.eulerAngles.z;
+    }
+
+    private void Update()
+    {
+        Quaternion targetTarget = Quaternion.Euler(0, 0, targetRotationZ);
+        gamesContent.transform.localRotation = Quaternion.Lerp(
+            gamesContent.transform.localRotation,
+            targetTarget,
+            Time.deltaTime * rotationSpeed
+        );
     }
 
     public void NextGame()
     {
         indexGame++;
-
         if (indexGame > allGames.Count - 1)
             indexGame = 0;
 
-        foreach (GameObject game in allGames)
-            game.SetActive(false);
-
-        allGames[indexGame].SetActive(true);
+        // Si tienes 4 objetos, cada paso es de 90 grados. 
+        // Usamos 360 / cantidad de objetos para que sea dinámico.
+        targetRotationZ -= 360f / allGames.Count;
     }
 
     public void PreviousGame()
     {
         indexGame--;
-
         if (indexGame < 0)
             indexGame = allGames.Count - 1;
 
-        foreach (GameObject game in allGames)
-            game.SetActive(false);
-
-        allGames[indexGame].SetActive(true);
+        targetRotationZ += 360f / allGames.Count;
     }
 }
